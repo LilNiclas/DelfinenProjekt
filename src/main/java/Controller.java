f2716044d7e9c145919cb055ab3e15d8635dd3ff
SimonBD1 <simo20k3@stud.kea.dk>
2022-11-23 12:01:39 +0100

    Lavet metoder til createMember, lavet testdata osv.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,11 @@
 public class Controller {
+    Database database = new Database();
+
+    public void createMember(String firstName, String lastName, String swimType, int age, boolean aktivitetsForm, int memberShipNumber) {
+        database.createMember(firstName, lastName, swimType, age, aktivitetsForm, memberShipNumber);
+    }
+
+    public void createTestData() {
+        database.createTestData();
+    }
 }
--- src/main/java/Userinterface.java
+++ src/main/java/Userinterface.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Userinterface {
@@ -5,20 +6,35 @@ public class Userinterface {
     public void Start () {
         System.out.println("Velkommen til Delfin svømmeklub");
         boolean menuError;
-        /*
+
         do {
             do {
                 startPage();
                 try {
                     int menuChoice = scanner.nextInt();
-                    if (menuChoice == 1){
-                        createSwimmer();
+                    if (menuChoice == 1) {
+                        createMember();
+                    } else if (menuChoice == 2) {
+                        editMembers();
+                    } else if (menuChoice == 3) {
+                        seeMembers();
+                    } else if (menuChoice == 4) {
+                        searchMembers();
+                    } else if (menuChoice == 5) {
+                        deleteMembers();
+                    } else if (menuChoice == 6) {
+                        saveMembers();
                     }
+                    menuError = false;
+                } catch (InputMismatchException ime) {
+                    System.out.println("Skriv kun tal");
+                    scanner.nextLine();
+                    menuError = true;
                 }
-            }
-        }
+            } while (menuError == true);
+        } while (true);
     }
-         */
+
     public void startPage() {
         System.out.println("""
                 Tast 1) for oprettelse af nye svømmere.
@@ -30,4 +46,64 @@ public class Userinterface {
                 Tast 9) for at afslutte.
                 """);
     }
+
+    public void createMember() {
+        scanner.nextLine();
+        System.out.println("Indtast fornavn på nyt medlem");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Indtast efternavn");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Indtast svømmetype(Motionist eller konkurrencesvømer)");
+        String swimType = scanner.nextLine();
+
+        System.out.println("Indtast alder");
+        int age = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Indtast brugers aktivitet i klubben(a=Aktivt eller p=passivt)");
+        boolean membershipStatus = true;
+        char activityAnswer;
+        do {
+            activityAnswer = scanner.nextLine().charAt(0);
+            if (activityAnswer == 'a') {
+                membershipStatus = true;
+                break;
+            } else if (activityAnswer == 'p') {
+                membershipStatus = false;
+                break;
+            } else
+                System.out.println("Tastefejl, tast aktiv eller passiv");
+
+        } while (activityAnswer != 'a' || activityAnswer != 'p');
+
+
+        System.out.println("Indtast medlemsnummer");
+        int memberShipNumber = Integer.parseInt(scanner.nextLine());
+
+        controller.createMember(firstName, lastName, swimType, age, membershipStatus, memberShipNumber);
+
+
+    }
+
+    public void editMembers() {
+
+    }
+
+    public void seeMembers() {
+        System.out.println("Liste over medlemmer: " + "\n" + "-".repeat(21));
+    }
+
+    public void searchMembers() {
+
+    }
+
+    public void deleteMembers() {
+
+    }
+
+    public void saveMembers() {
+
+    }
+
 }
